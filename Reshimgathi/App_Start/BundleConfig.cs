c78ee5abf129f74b1b3408ca2afca6105f58b75f
Amit Patange <amitpatange88@gmail.com>
2019-02-12 23:03:35 +0530

    registration files added

--- Reshimgathi/App_Start/BundleConfig.cs
+++ Reshimgathi/App_Start/BundleConfig.cs
@@ -8,26 +8,26 @@ namespace Reshimgathi
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //Script Bundle JS
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new StyleBundle("~/Base/js").Include(
+            bundles.Add(new ScriptBundle("~/Base/js").Include(
                       "~/js/bootstrap.js",
                       "~/js/config.js",
                       "~/js/jquery-1.11.1.min.js",
                       "~/js/jquery.swipebox.min.js",
                       "~/js/jquery.flexslider.js"));
 
-            bundles.Add(new StyleBundle("~/Facebook/JS").Include(
+            bundles.Add(new ScriptBundle("~/Facebook/JS").Include(
                       "~/js/Facebook.js"));
 
-            bundles.Add(new StyleBundle("~/Twitter/JS").Include(
-                      "~/js/Twitter.js",
-                      "~/js/oauth.js"));
+            bundles.Add(new ScriptBundle("~/Twitter/JS").Include(
+                      "~/js/Twitter.js"));
 
-            bundles.Add(new StyleBundle("~/Gmail/JS").Include(
+            bundles.Add(new ScriptBundle("~/Gmail/JS").Include(
                 "~/js/Gmail.js"));
-
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -37,6 +37,11 @@ namespace Reshimgathi
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/SmartWizard/JS").Include(
+                      "~/js/jquery.smartWizard.min.js"));
+
+            //Style Bundle CSS
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
@@ -50,6 +55,12 @@ namespace Reshimgathi
             bundles.Add(new StyleBundle("~/Twitter/CSS").Include(
                       "~/css/bootstrap-social.min.css",
                       "~/css/font-awesome.min.css"));
+
+            bundles.Add(new StyleBundle("~/SmartWizard/CSS").Include(
+                      "~/css/smart_wizard.css",
+                       "~/css/smart_wizard_theme_circles.css",
+                        "~/css/smart_wizard_theme_dots.css",
+                      "~/css/smart_wizard_theme_arrows.css"));
 
         }
     }
