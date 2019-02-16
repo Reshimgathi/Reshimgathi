c5b6760181fbd9fda79fef83e4ac5b93c3b7aa67
Amit Patange <amitpatange88@gmail.com>
2019-02-16 19:40:48 +0530

    profile and account setting pages added

---
+++ Reshimgathi/Controllers/AccountSettingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reshimgathi.Controllers
+{
+    public class AccountSettingController : Controller
+    {
+        // GET: AccountSetting
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VerifyMobile()
+        {
+            return View();
+        }
+
+        public ActionResult VerifyEmail()
+        {
+            return View();
+        }
+    }
+}
---
+++ Reshimgathi/Controllers/ProfileSettingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reshimgathi.Controllers
+{
+    public class ProfileSettingController : Controller
+    {
+        // GET: ProfileSetting
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Shortlisted()
+        {
+            return View();
+        }
+
+        public ActionResult Interested()
+        {
+            return View();
+        }
+    }
+}
